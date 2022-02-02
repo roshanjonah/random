2f0b26ded1310d2af8818010cf8eb4f8d1563a89
Chandran <chandrantwins@gmail.com>
2022-02-02 23:45:38 +0100

    Update mwz-mach.js

--- mwz-mach.js
+++ mwz-mach.js
@@ -1,31 +1,30 @@
-$(document).ready(function () {
-  $(':input[type="submit"]').prop("disabled", true);
-  $apiReturn = false;
-  //API will called after license field out of focus
-  $("input#element_10").on("blur", function () {
-    $license = $("input#element_10").val();
-    $.ajax({
-      url: "https://forms.janz.co/mailwizz.php",
-      method: "POST",
-      data: { 'purchase_code': $license },
-      processData: false,
-      contentType: false,
-      success: function (json) {
-        console.log(json);
-        var response = JSON.parse(json);
-        if (response.status == "success") {
-          $apiReturn = true;
-        } else {
-          $apiReturn = false;
+$(document).ready(function() {
+    $(':input[type="submit"]').prop('disabled', true);
+	$apiReturn = false;
+    //API will called after license field out of focus
+    $('input#element_10').on('blur',function() {        
+		var fmData = new FormData();
+		fmData.append('purchase_code', $('input#element_10').val());
+		$.ajax({
+			url: 'https://form.janz.co/mailwizz.php',
+			method: 'POST',
+			data: fmData, 
+			processData: false, 
+			contentType: false, 
+			success: function(json){
+				console.log(json);
+				var response = JSON.parse(json);
+				if (response.status == 'success') {
+					$apiReturn = true;
+				} else {
+					$apiReturn = false;
+				}
+			}, 
+			error: function(err){console.log(err)} 
+		});
+        //put the API call here to get the license status
+        if($apiReturn) {
+            $(':input[type="submit"]').prop('disabled', false);
         }
-      },
-      error: function (err) {
-        console.log(err);
-      },
     });
-    //put the API call here to get the license status
-    if ($apiReturn) {
-      $(':input[type="submit"]').prop("disabled", false);
-    }
-  });
 });
